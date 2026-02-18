c5adb4f988cf542aaaf0f92d930000f7a474f774
Lebi JM <jaafarmerhi1@hotmail.co.uk>
2026-02-18 19:00:17 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+url1 = "https://thecardvault.co.uk/collections/one-piece-tcg-all-products?sort_by=created-descending&filter.p.m.custom.availability=In+Stock&filter.p.m.custom.availability=Pre-Order"
+
+xyz = BeautifulSoup()
+
+print(xyz.prettify())
